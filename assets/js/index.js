3aed7a9839e4288b4ecd52111c3da2ba821c145c
umarovali <umarovali.345@gmail.com>
2026-01-29 00:29:09 +0600

    benefits section created

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,23 @@ document.addEventListener('DOMContentLoaded', () => {
             pauseOnMouseEnter: false,
         },
 
-        speed: 800, 
+        speed: 800,
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const benefitsSwiper = new Swiper('.benefits-brands-swiper', {
+        loop: true,
+        slidesPerView: 'auto',
+        spaceBetween: 10,
+        speed: 8000,
+        freeMode: true,
+        freeModeMomentum: false,
+        autoplay: {
+            delay: 0,
+            disableOnInteraction: false,
+        },
+        allowTouchMove: false,
+    });
+});
+
